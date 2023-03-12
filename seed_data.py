09654e7004ca188e3463ab26fd844fd694a30a3c
ELDARCOMPUTER14\Eldar-IT <EldarComputer14>
2023-03-11 22:24:21 -0300

    Formularios de carga y busqueda

---
+++ seed_data.py
@@ -0,0 +1,27 @@
+from SocialTravel.models import Post
+
+Post (carousel_caption_title = "Un Carousel Title1",
+carousel_caption_description = "Un Carousel descript1",
+heading = "Mi viaje1",
+description="una description1",
+
+).save()
+
+Post (carousel_caption_title = "Un Carousel Title2",
+carousel_caption_description = "Un Carousel descript2",
+heading = "Mi viaje2",
+description="una description2",
+).save()
+
+Post (carousel_caption_title = "Un Carousel Title3",
+carousel_caption_description = "Un Carousel descript3",
+heading = "Mi viaje3",
+description="una description3",
+
+).save()
+
+Post (carousel_caption_title = "Un Carousel Title4",
+carousel_caption_description = "Un Carousel descript4",
+heading = "Mi viaje4",
+description="una description4",
+).save()
